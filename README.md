ca4c5648542adde5fcf905092cb1a5ee283c7b17
Peter DeMartini <peterdemartini@me.com>
2014-05-23 14:36:50 -0700

    Working version

--- README.md
+++ README.md
@@ -1,6 +1,49 @@
 Github Calendar JS
 ==================
 
-Github contributions calendar using D3.
+Github contributions calendar using D3 and [Cal-heatmap](http://kamisama.github.io/cal-heatmap/v2)
 
-**Incomplete**
+### Install
+
+1. Spin up a [Github Proxy](https://github.com/peterdemartini/github-proxy) on Heroku.
+
+2. Install jQuery, moment, d3, and Cal-heatmap.
+
+### Example
+
+    <!DOCTYPE html>
+    <html lang="en">
+
+        <head>
+            <meta charset="utf-8">
+            <meta http-equiv="X-UA-Compatible" content="IE=edge">
+            <meta name="viewport" content="width=device-width, initial-scale=1">
+            <title>Github Calendar Test</title>
+
+            <link href="cal-heatmap/cal-heatmap.css" rel="stylesheet">
+        </head>
+
+        <body>
+
+            <h1>Github Calendar Test</h1>
+
+            <div id="github-cal"></div>
+
+            <script src="jquery/jquery.min.js"></script>
+            <script src="moment/moment.js"></script>
+            <script src="d3/d3.min.js"></script>
+            <script src="cal-heatmap/cal-heatmap.min.js"></script>
+            <script src="github-cal.js"></script>
+
+            <script>
+                $(document).ready(function(){
+                    githubCal.init('github-cal', {
+                        proxy: [proxy url],
+                        username: [github username]
+                    });
+                });
+            </script>
+
+        </body>
+
+    </html>
--- github-cal.js
+++ github-cal.js
@@ -1,180 +1,74 @@
 'use strict';
 
-window.githubCal = (function (global, $, d3) {
+window.githubCal = (function (global, $, moment, CalHeatMap) {
 
     var obj = {
-        selector: '#github-cal',
+        id: 'github-cal',
         options: {
-            margin : {
-                top: 50,
-                right: 0,
-                bottom: 100,
-                left: 30
-            },
-            width : 960,
-            height: 300,
-            // Ordered from less to more contributions
-            colors: [
-                '#eee',
-                '#d6e685',
-                '#8cc665',
-                '#44a340',
-                '#1e6823'
-            ],
-            days: [
-                'Mo',
-                'Tu',
-                'We',
-                'Th',
-                'Fr',
-                'Sa',
-                'Su'
-            ],
-            months: [
-                'Jan',
-                'Feb',
-                'Mar',
-                'Apr',
-                'May',
-                'Jun',
-                'Jul',
-                'Aug',
-                'Sep',
-                'Oct',
-                'Nov',
-                'Dec'
-            ],
-            buckets: 5,
-            username : 'peterdemartini'
+            proxy: 'http://stormy-sea-4131.herokuapp.com',
+            username: 'peterdemartini'
         },
         data: [],
+        legend: [10, 20, 30, 40],
         setOptions: function (opts) {
             obj.options = $.extend(obj.options, opts);
-
-            obj.options.width = obj.options.width - obj.options.margin.left - obj.options.margin.right;
-            obj.options.height = obj.options.height - obj.options.margin.top - obj.options.margin.bottom;
-
-            obj.options.gridSize = 30;
-            obj.options.legendElementWidth = obj.options.gridSize;
         },
-        init: function (selector, opts) {
-            obj.selector = selector;
+        init: function (id, opts) {
+            obj.id = id;
             obj.setOptions(opts || {});
-            obj.getData(function(data){
-                data = data.map(function(d){
-                    var date = new Date(d[0]);
-                    return {
-                        day : date.getDay(),
-                        month : date.getMonth(),
-                        value : d[1]
-                    };
+            obj.getData(function (res) {
+                var data = {},
+                    min = 0,
+                    max = 0;
+                res.forEach(function (d) {
+                    var date = moment(d[0], 'YYYY/MM/DD').unix();
+                    var value = d[1];
+                    if(value < min){
+                        min = value;
+                    }
+                    if(value > max){
+                        max = value;
+                    }
+                    if(value){
+                        if(data[date])
+                            data[date] += value;
+                        else
+                            data[date] = value;
+                    }
                 });
 
+                var per =  Math.floor(max / obj.legend.length); //23 / 4 = 5
+                var inc = 0;
+                for(var x = 0; x < obj.legend.length; x++){
+                    obj.legend[x] = inc = inc + per;
+                }
                 obj.data = data;
                 obj.generate();
             });
 
         },
         getData: function (cb) {
-            $.get('http://stormy-sea-4131.herokuapp.com/contributions/' + obj.options.username, cb);
+            $.get(obj.options.proxy + '/contributions/' + obj.options.username, cb);
         },
         generate: function () {
-            var colorScale = d3.scale.quantile()
-                .domain([0, obj.options.buckets - 1, d3.max(obj.data, function (d) {
-                    return d.value;
-                })])
-                .range(obj.options.colors);
-
-            var svg = d3.select(obj.selector).append('svg')
-                .attr('width', obj.options.width + obj.options.margin.left + obj.options.margin.right)
-                .attr('height', obj.options.height + obj.options.margin.top + obj.options.margin.bottom)
-                .append('g')
-                .attr('transform', 'translate(' + obj.options.margin.left + ',' + obj.options.margin.top + ')');
-
-            svg.selectAll('.dayLabel')
-                .data(obj.options.days)
-                .enter().append('text')
-                .text(function (d) {
-                    return d;
-                })
-                .attr('x', 0)
-                .attr('y', function (d, i) {
-                    return i * obj.options.gridSize;
-                })
-                .style('text-anchor', 'end')
-                .attr('transform', 'translate(-6,' + obj.options.gridSize / 1.5 + ')')
-                .attr('class', function (d, i) {
-                    return ((i >= 0 && i <= 4) ? 'dayLabel mono axis axis-workweek' : 'dayLabel mono axis');
-                });
-
-            svg.selectAll('.monthLabel')
-                .data(obj.options.months)
-                .enter().append('text')
-                .text(function (d) {
-                    return d;
-                })
-                .attr('x', function (d, i) {
-                    return i * obj.options.gridSize;
-                })
-                .attr('y', 0)
-                .style('text-anchor', 'middle')
-                .attr('transform', 'translate(' + obj.options.gridSize / 2 + ', -6)')
-                .attr('class', function (d, i) {
-                    return ((i >= 7 && i <= 16) ? 'monthLabel mono axis axis-worktime' : 'monthLabel mono axis');
-                });
-
-            var heatMap = svg.selectAll('.month')
-                .data(obj.data)
-                .enter().append('rect')
-                .attr('x', function (d) {
-                    return d.month  * obj.options.gridSize;
-                })
-                .attr('y', function (d) {
-                    return d.day * obj.options.gridSize;
-                })
-                .attr('rx', 4)
-                .attr('ry', 4)
-                .attr('class', 'month bordered')
-                .attr('width', obj.options.gridSize)
-                .attr('height', obj.options.gridSize)
-                .style('fill', obj.options.colors[0]);
-
-            heatMap.transition().duration(1000)
-                .style('fill', function (d) {
-                    return colorScale(d.value);
-                });
-
-            heatMap.append('title').text(function (d) {
-                return d.value;
+            var calendar = new CalHeatMap();
+            calendar.init({
+                data: obj.data,
+                start: moment().subtract('months', 11).toDate(),
+                end: new Date(),
+                id: obj.id,
+                domain: 'month',
+                subDomain: 'day',
+                range: 12,
+                scale: [2, 4, 6, 8, 10],
+                legend : obj.legend,
+                format: {
+                    date: function (date) {
+                        return moment(date).format('LL');
+                    },
+                    legend: null,
+                }
             });
-
-            var legend = svg.selectAll('.legend')
-                .data([0].concat(colorScale.quantiles()), function (d) {
-                    return d;
-                })
-                .enter().append('g')
-                .attr('class', 'legend');
-
-            legend.append('rect')
-                .attr('x', function (d, i) {
-                    return obj.options.legendElementWidth * i;
-                })
-                .attr('y', obj.options.height)
-                .attr('width', obj.options.legendElementWidth)
-                .attr('height', obj.options.gridSize / 2)
-                .style('fill', function (d, i) {
-                    return obj.options.colors[i];
-                });
-
-            legend.append('text')
-                .attr('class', 'mono')
-                .text(function (d) {
-                    return '≥ ' + Math.round(d);
-                })
-                .attr('x', function (d, i) {
-                    return obj.options.legendElementWidth * i;
-                })
-                .attr('y', obj.options.height + obj.options.gridSize);
         }
     };
 
@@ -184,4 +78,4 @@ window.githubCal = (function (global, $, d3) {
     };
 
     return publicAPI;
-})(window, jQuery, d3);
+})(window, jQuery, moment, CalHeatMap);
